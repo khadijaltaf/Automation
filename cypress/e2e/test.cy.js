6c3e632e81b5dcabf1155d2846118a8071c1eb38
khadijaltaf <khadija0fficial40@gmail.com>
2024-09-27 15:55:43 +0500

    setup Cypress for mymo and also created one test case 'opening website'

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,9 @@
+describe('MyMo Login Automation', () => {
+  it('Logs into MyMo using Google account', () => {
+    // Visit the MyMo application
+    cy.visit('https://app-dev.mymo.work')
+
+    
+    
+  })
+})
